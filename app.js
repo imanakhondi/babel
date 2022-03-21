39cc0f3420513bcfb794ee3740c3de3b4a55a93b
imankd <iman@ex@.com>
2022-03-22 01:45:41 +0430

    all file changed

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 const userName = "iman";
+const userEmail="iman@ex.com";
 const getUser = () => console.log("iman is developer");
 class App{
     getName(){
--- bundle.js
+++ bundle.js
@@ -7,6 +7,7 @@ function _defineProperties(target, props) { for (var i = 0; i < props.length; i+
 function _createClass(Constructor, protoProps, staticProps) { if (protoProps) _defineProperties(Constructor.prototype, protoProps); if (staticProps) _defineProperties(Constructor, staticProps); Object.defineProperty(Constructor, "prototype", { writable: false }); return Constructor; }
 
 var userName = "iman";
+var userEmail = "iman@ex.com";
 
 var getUser = function getUser() {
   return console.log("iman is developer");
